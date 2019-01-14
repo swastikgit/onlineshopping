c8842d7b90c8aa73968730253a40381ba6e4093b
Ashok Balami <AshokBalami@hotmail.com>
2019-01-13 23:20:03 -0500

    initial commit for online shopping application

---
+++ shoping/src/main/java/com/asok/example/shoping/ShopingApplication.java
@@ -0,0 +1,14 @@
+package com.asok.example.shoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopingApplication.class, args);
+	}
+
+}
+
---
+++ shoping/src/test/java/com/asok/example/shoping/ShopingApplicationTests.java
@@ -0,0 +1,17 @@
+package com.asok.example.shoping;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
